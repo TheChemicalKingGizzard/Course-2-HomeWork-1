5ca0492fcefa08158c4bd97720ba986f7517d480
DaniilPetrov <dany_petrov@icloud.com>
2021-07-31 22:04:21 +0300

    Initial Commit

---
+++ Course 2 HW 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Course 2 HW 1
+//
+//  Created by Даниил Петров on 31.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
